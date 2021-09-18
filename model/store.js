1f4e65b8ec78718556fcf4a1f2cf52f46a03f87f
khadetou <khadetou96@gmail.com>
2021-09-18 23:33:49 +0000

    model config

---
+++ model/store.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const storeSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    address: {
+      type: String,
+    },
+    number: {
+      type: String,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Store || mongoose.model("Store", storeSchema);
---
+++ model/user.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: [true, "Enter your firstname !"],
+    maxLength: [50, "Your name cannot exceed more than 50 characters"],
+  },
+  name: {
+    type: String,
+    required: [true, "Enter your lastname !"],
+    maxLength: [50, "Your name cannot exceed more than 50 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Enter your email !"],
+    unique: true,
+    validate: [validator.isEmail, "Enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Enter your password !"],
+    minLength: [6, "Your password must do at least 6 characters"],
+    select: false,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: [String],
+    enum: ["User", "Seller", "Admin"],
+    default: "user",
+  },
+  phoneNumber: {
+    type: String,
+  },
+  idcard: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  resetPasswordToken: String,
+  resetPasswordExpired: Date,
+});
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
